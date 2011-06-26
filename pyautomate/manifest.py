9fab77451007708b7674f7cc5b99c1de33fff5c4
Tim Diels <limyreth@gmail.com>
2011-06-26 19:07:13 +0200

    Manifest digest now ignores modification time of files
    This was necessary to avoid problems with testing it.

--- pyautomate/manifest.py
+++ pyautomate/manifest.py
@@ -13,55 +13,56 @@ class _ManifestAlgorithm(object):
         self.name = name
 
     def generate_manifest(self, root):
-        def recurse(sub):
+        def recurse(subdir):
             # To ensure that a line-by-line comparison of the manifests
             # is possible, we require that filenames don't contain newlines.
             # Otherwise, you can name a file so that the part after the \n
             # would be interpreted as another line in the manifest.
-            if '\n' in sub: raise Exception("Newline in filename '%s'" % sub)
-            assert sub.startswith('/')
+            if '\n' in subdir: raise Exception("Newline in filename '%s'" %
+                                               subdir)
+            assert subdir.startswith('/')
 
-            full = os.path.join(root, sub[1:])
-            info = os.lstat(full)
-            new_digest = self.new_digest
+            full = os.path.join(root, subdir[1:])
             
-            m = info.st_mode
-            if not stat.S_ISDIR(m): raise Exception('Not a directory: "%s"' % full)
-            if sub != '/':
-                yield "D %s" % sub
+            if not os.path.isdir(full): 
+                raise Exception('Not a directory: "%s"' % full)
+
+            if subdir != '/':
+                yield "D %s" % subdir
+
             items = os.listdir(full)
             items.sort()
             dirs = []
-            for leaf in items:
-                path = os.path.join(root, sub[1:], leaf)
+            for item in items:
+                path = os.path.join(root, subdir[1:], item)
                 info = os.lstat(path)
                 m = info.st_mode
 
-                if stat.S_ISREG(m):
-                    if leaf == '.manifest': continue
+                new_digest = self.new_digest
 
+                if stat.S_ISREG(m):
                     d = digest(path, new_digest)
                     if m & 0o111:
-                        yield "X %s %s %s %s" % (d, int(info.st_mtime), info.st_size, leaf)
+                        yield "X %s %s %s" % (d, info.st_size, item)
                     else:
-                        yield "F %s %s %s %s" % (d, int(info.st_mtime), info.st_size, leaf)
+                        yield "F %s %s %s" % (d, info.st_size, item)
                 elif stat.S_ISLNK(m):
                     target = os.readlink(path)
                     d = new_digest(target).hexdigest()
                     # Note: Can't use utime on symlinks, so skip mtime
                     # Note: eCryptfs may report length as zero, so count ourselves instead
-                    yield "S %s %s %s" % (d, len(target), leaf)
+                    yield "S %s %s %s" % (d, len(target), item)
                 elif stat.S_ISDIR(m):
-                    dirs.append(leaf)
+                    dirs.append(item)
                 else:
                     raise Exception("Unknown object '%s' (not a file, directory or symlink)" %
                             path)
 
-            if not sub.endswith('/'):
-                sub += '/'
+            if not subdir.endswith('/'):
+                subdir += '/'
             for x in dirs:
-                # Note: "sub" is always Unix style. Don't use os.path.join here.
-                for y in recurse(sub + x): yield y
+                # Note: "subdir" is always Unix style. Don't use os.path.join here.
+                for y in recurse(subdir + x): yield y
             return
 
         for x in recurse('/'): yield x
